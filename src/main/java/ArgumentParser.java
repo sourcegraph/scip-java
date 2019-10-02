b0403fbd2bc05c14fcefd5994ab03681e1f6a8b2
Eric Fritz <eric@eric-fritz.com>
2019-10-02 08:45:44 -0500

    Update arguments.

--- src/main/java/ArgumentParser.java
+++ src/main/java/ArgumentParser.java
@@ -1,12 +1,10 @@
 import org.apache.commons.cli.*;
 
-import java.io.FileNotFoundException;
-
 public class ArgumentParser {
-    public static Arguments parse(String[] args) throws FileNotFoundException {
-        // TODO - version
-        // TODO - description (lsif-java is an LSIF indexer for Java.)
+    public static final String VERSION = "0.1.0";
+    public static final String PROTOCOL_VERSION = "0.4.0";
 
+    public static Arguments parse(String[] args) {
         Options options = createOptions();
         CommandLineParser parser = new DefaultParser();
         HelpFormatter formatter = new HelpFormatter();
@@ -16,9 +14,19 @@ public class ArgumentParser {
             cmd = parser.parse(options, args);
         } catch (ParseException e) {
             System.out.println(e.getMessage());
-            formatter.printHelp("lsif-java", options);
+            formatter.printHelp("lsif-java","lsif-java is an LSIF indexer for Java.\n\n", options, "");
             System.exit(1);
             return null;
+        }
+
+        if (cmd.hasOption("help")) {
+            formatter.printHelp("lsif-java","lsif-java is an LSIF indexer for Java.\n\n", options, "");
+            System.exit(0);
+        }
+
+        if (cmd.hasOption("version")) {
+            System.out.printf("%s, protocol version %s\n", VERSION, PROTOCOL_VERSION);
+            System.exit(0);
         }
 
         boolean debug = cmd.hasOption("debug");
@@ -45,32 +53,47 @@ public class ArgumentParser {
         Options options = new Options();
 
         options.addOption(new Option(
-                "", "debug", false,
+                "help", false,
+                "Show help."
+        ));
+
+        options.addOption(new Option(
+                "version", false,
+                "Show version."
+        ));
+
+        options.addOption(new Option(
+                "debug", false,
                 "Display debug information."
         ));
 
         options.addOption(new Option(
-                "", "verbose", false,
+                "verbose", false,
                 "Display verbose information."
         ));
 
         options.addOption(new Option(
-                "", "projectRoot", true,
+                "projectRoot", true,
                 "Specifies the project root. Defaults to the current working directory."
         ));
 
         options.addOption(new Option(
-                "", "noContents", false,
+                "noContents", false,
                 "File contents will not be embedded into the dump."
         ));
 
         options.addOption(new Option(
-                "", "out", true,
+                "out", true,
                 "The output file the dump is save to."
         ));
 
         options.addOption(new Option(
-                "", "stdout", false,
+                "stdout", false,
+                "Writes the dump to stdout."
+        ));
+
+        options.addOption(new Option(
+                "stdout", false,
                 "Writes the dump to stdout."
         ));
 
