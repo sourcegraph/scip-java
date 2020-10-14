9b46e8191b824426276ac80f37de79017c6f7916
Noah Santschi-Cooney <noah@santschi-cooney.ch>
2020-10-14 12:14:14 +0100

    Java emitter to Kotlin emitter for that super sweet non-random json keys output

---
+++ src/main/kotlin/lsifjava/Emitter.kt
@@ -0,0 +1,32 @@
+package lsifjava
+
+import java.io.PrintWriter
+
+import com.google.gson.Gson
+
+class Emitter(val writer: PrintWriter) {
+    private var nextId: Int = 1
+
+    private val gson: Gson = Gson()
+
+    fun emitVertex(labelName: String, args: Map<String, Any>) = emit("vertex", labelName, args)
+
+    fun emitEdge(labelName: String, args: Map<String, Any>) = emit("edge", labelName, args)
+
+    private fun emit(typeName: String, labelName: String, args: Map<String, Any>): String {
+        val id = (nextId++).toString()
+
+        val map = mutableMapOf<String, Any>(
+                "id" to id,
+                "type" to typeName,
+                "label" to labelName
+        )
+        map.putAll(args)
+
+        writer.println(gson.toJson(map))
+
+        return id
+    }
+
+    fun numElements() = nextId
+}
