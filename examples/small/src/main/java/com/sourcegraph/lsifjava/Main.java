719b3afe55ed4e3e466d5f778a0c6db0f2399ab4
Noah Santschi-Cooney <noah@santschi-cooney.ch>
2021-02-03 21:34:38 +0000

    Added basic gradle example for Olaf

--- examples/small/src/main/java/com/sourcegraph/lsifjava/Main.java
+++ examples/small/src/main/java/com/sourcegraph/lsifjava/Main.java
@@ -1,11 +1,19 @@
 package com.sourcegraph.lsifjava;
 
+import com.google.gson.FieldNamingPolicy;
+import java.util.ArrayList;
+
 /**
  * some Main stuff
  */
 public abstract class Main {
     public static void main(String[] args) {
         Sample s = new Sample();
+
+        //ArrayList<String> list = new ArrayList<>(3);
+
+        //Gson g = new Gson();
+        FieldNamingPolicy x = FieldNamingPolicy.IDENTITY;
         //Sample.nom();
     }
 }
