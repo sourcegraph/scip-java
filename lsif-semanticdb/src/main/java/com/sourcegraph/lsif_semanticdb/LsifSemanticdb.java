26091278ce4b700b5c64f71f75b5ce91f09cb341
Noah Santschi-Cooney <noah@santschi-cooney.ch>
2021-04-14 00:33:07 +0100

    link overrides to overridden symbol's referenceResult

--- lsif-semanticdb/src/main/java/com/sourcegraph/lsif_semanticdb/LsifSemanticdb.java
+++ lsif-semanticdb/src/main/java/com/sourcegraph/lsif_semanticdb/LsifSemanticdb.java
@@ -136,6 +136,18 @@ public class LsifSemanticdb {
           writer.emitHoverEdge(ids.resultSet, hoverId);
         }
       }
+
+      // Overrides
+      if (symbolInformation.getOverriddenSymbolsCount() > 0) {
+        int[] overriddenReferenceResultIds = new int[symbolInformation.getOverriddenSymbolsCount()];
+        for (int i = 0; i < symbolInformation.getOverriddenSymbolsCount(); i++) {
+          String overriddenSymbol = symbolInformation.getOverriddenSymbols(i);
+          ResultIds overriddenIds = results.getOrInsertResultSet(overriddenSymbol);
+          overriddenReferenceResultIds[i] = overriddenIds.referenceResult;
+        }
+        writer.emitReferenceResultsItemEdge(
+            ids.referenceResult, overriddenReferenceResultIds, doc.id);
+      }
     }
     writer.emitContains(doc.id, new ArrayList<>(rangeIds));
     writer.flush();
