ae26e1799ca8bb165114d3cdbf8d1dd7d8610329
Win Wang <winw@twitter.com>
2021-05-13 20:19:54 -0400

    Allow javac plugin to infer targetroot from javac class output directory

--- semanticdb-javac/src/main/java/com/sourcegraph/semanticdb_javac/SemanticdbPlugin.java
+++ semanticdb-javac/src/main/java/com/sourcegraph/semanticdb_javac/SemanticdbPlugin.java
@@ -1,8 +1,10 @@
 package com.sourcegraph.semanticdb_javac;
 
-import com.sun.source.util.*;
+import com.sun.source.util.JavacTask;
+import com.sun.source.util.Plugin;
 import com.sun.tools.javac.api.BasicJavacTask;
 import com.sun.tools.javac.model.JavacTypes;
+import com.sun.tools.javac.util.Context;
 
 /** Entrypoint of the semanticdb-javac compiler plugin. */
 public class SemanticdbPlugin implements Plugin {
@@ -14,10 +16,12 @@ public class SemanticdbPlugin implements Plugin {
 
   @Override
   public void init(JavacTask task, String... args) {
+    Context ctx = ((BasicJavacTask) task).getContext();
+
     SemanticdbReporter reporter = new SemanticdbReporter();
-    SemanticdbJavacOptions options = SemanticdbJavacOptions.parse(args);
+    SemanticdbJavacOptions options = SemanticdbJavacOptions.parse(args, ctx);
     GlobalSymbolsCache globals = new GlobalSymbolsCache(options);
-    JavacTypes javacTypes = JavacTypes.instance(((BasicJavacTask) task).getContext());
+    JavacTypes javacTypes = JavacTypes.instance(ctx);
     if (!options.errors.isEmpty()) {
       for (String error : options.errors) {
         reporter.error(error);
--- tests/unit/src/main/scala/tests/SimpleFileManager.java
+++ tests/unit/src/main/scala/tests/SimpleFileManager.java
@@ -1,31 +1,36 @@
 package tests;
 
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.List;
 import javax.tools.FileObject;
 import javax.tools.ForwardingJavaFileManager;
 import javax.tools.JavaFileObject;
 import javax.tools.StandardJavaFileManager;
-import java.net.URI;
-import java.util.ArrayList;
-import java.util.List;
+
+import com.sourcegraph.semanticdb_javac.SemanticdbJavacOptions;
 
 public class SimpleFileManager
-        extends ForwardingJavaFileManager<StandardJavaFileManager> {
+    extends ForwardingJavaFileManager<StandardJavaFileManager> {
 
-    public List<SimpleClassFile> compiled = new ArrayList<>();
-    protected SimpleFileManager(StandardJavaFileManager fileManager) {
-        super(fileManager);
+  public List<SimpleClassFile> compiled = new ArrayList<>();
+
+  protected SimpleFileManager(StandardJavaFileManager fileManager) {
+    super(fileManager);
+  }
+
+  // standard constructors/getters
+
+  @Override
+  public JavaFileObject getJavaFileForOutput(Location location,
+                                             String className, JavaFileObject.Kind kind, FileObject sibling) {
+    SimpleClassFile result = new SimpleClassFile(
+        URI.create("string://" + className));
+    if (!className.equals(SemanticdbJavacOptions.stubClassName)) {
+      compiled.add(result);
     }
-
-    // standard constructors/getters
-
-    @Override
-    public JavaFileObject getJavaFileForOutput(Location location,
-                                               String className, JavaFileObject.Kind kind, FileObject sibling) {
-        SimpleClassFile result = new SimpleClassFile(
-                URI.create("string://" + className));
-        compiled.add(result);
-        return result;
-    }
+    return result;
+  }
 
 }
 
