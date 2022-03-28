5d7ccd55d13282980b843c94dbe5ce88786e2cc3
Noah S-C <noah@santschi-cooney.ch>
2022-03-28 23:30:00 +0100

    copy JDK_HOME bootstrap files to forwarding toolchain directory for Kotlin (#418)
    Co-authored-by: Varun Gandhi <varun.gandhi@sourcegraph.com>

--- lsif-java/src/main/scala/com/sourcegraph/lsif_java/buildtools/GradleJavaCompiler.scala
+++ lsif-java/src/main/scala/com/sourcegraph/lsif_java/buildtools/GradleJavaCompiler.scala
@@ -2,6 +2,7 @@ package com.sourcegraph.lsif_java.buildtools
 
 import java.nio.charset.StandardCharsets
 import java.nio.file.Files
+import java.nio.file.NoSuchFileException
 import java.nio.file.Path
 import java.nio.file.Paths
 import java.nio.file.StandardCopyOption
@@ -70,9 +71,9 @@ case class GradleJavaCompiler(languageVersion: String, javacPath: Path) {
     val javaCommand = ListBuffer[String](
       javaBinary.toString,
       s"-javaagent:$agent",
-      s"-Dsemanticdb.javacopts=${javacopts}",
-      s"-Dsemanticdb.pluginpath=${pluginPath}",
-      s"-Dsemanticdb.targetroot=${targetroot}",
+      s"-Dsemanticdb.javacopts=$javacopts",
+      s"-Dsemanticdb.pluginpath=$pluginPath",
+      s"-Dsemanticdb.targetroot=$targetroot",
       s"-Dsemanticdb.sourceroot=${index.workingDirectory}"
     )
     if (index.verbose) {
@@ -98,6 +99,39 @@ case class GradleJavaCompiler(languageVersion: String, javacPath: Path) {
       )
       .toFile
       .setExecutable(true)
+
+    val copyFiles =
+      (source: Path, destination: Path) => {
+        Files
+          .walk(source)
+          .forEach(t => {
+            val destPath = destination.resolve(source.relativize(t))
+            try {
+              Files.copy(t, destPath)
+            } catch {
+              case _: NoSuchFileException =>
+                return
+            }
+          })
+      }
+
+    // For compile{Test}Kotlin when using jvm toolchains, we need to have access
+    // to JDK internals found in <java-installation-path>/lib in JDK 9+,
+    // as well as <java-installation-path>/jre/lib in JDK <=8, else we get
+    // "no class roots are found in the JDK path" from the compile{Test}Kotlin tasks.
+    // https://docs.oracle.com/en/java/javase/12/migrate/index.html#JSMIG-GUID-A78CC891-701D-4549-AA4E-B8DD90228B4B
+    val javaHome = javacPath.getParent.getParent
+    val libPath = dir.resolve("lib")
+    val javacLibPath = javaHome.resolve("lib")
+    copyFiles(javacLibPath, libPath)
+
+    if (languageVersion == "8") {
+      val jreLibPath = dir.resolve("jre").resolve("lib")
+      Files.createDirectories(jreLibPath.getParent)
+      val javacJreLibPath = javaHome.resolve("jre").resolve("lib")
+
+      copyFiles(javacJreLibPath, jreLibPath)
+    }
   }
 }
 object GradleJavaCompiler {
@@ -111,9 +145,9 @@ object GradleJavaCompiler {
   /**
    * Parses a single space-separated line into a GradleJavaCompiler instance.
    *
-   * Example input: "8 /path/javac"
+   * Example input: "8 /javacLibPath/javac"
    *
-   * Example output: `Some(GradleJavaCompiler("8", * /path/javac))`
+   * Example output: `Some(GradleJavaCompiler("8", * /javacLibPath/javac))`
    */
   def fromLine(line: String): Option[GradleJavaCompiler] =
     line.split(' ') match {
