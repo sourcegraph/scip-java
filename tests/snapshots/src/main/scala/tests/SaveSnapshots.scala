f8f12f658a31d0a9127de2af90217864d4a0af95
Anton Sviridov <anton.sviridov@sourcegraph.com>
2024-08-06 10:09:59 +0100

    Allow regenerating a subset of snapshots

--- tests/snapshots/src/main/scala/tests/SaveSnapshots.scala
+++ tests/snapshots/src/main/scala/tests/SaveSnapshots.scala
@@ -3,7 +3,19 @@ package tests
 object SaveSnapshots {
   def main(args: Array[String]): Unit = {
     val expectDirectory = tests.snapshots.BuildInfo.snapshotDirectory.toPath
-    SemanticdbJavacSnapshotGenerator
-      .run(SnapshotContext(expectDirectory), new SaveSnapshotHandler)
+    val mapping = Map(
+      "minimized" -> new MinimizedSnapshotScipGenerator(),
+      "library" -> new LibrarySnapshotGenerator()
+    )
+
+    val enabledGenerators =
+      if (args.isEmpty)
+        mapping.values.toList
+      else
+        args.flatMap(mapping.get).toList
+
+    val generator = new AggregateSnapshotGenerator(enabledGenerators)
+
+    generator.run(SnapshotContext(expectDirectory), new SaveSnapshotHandler)
   }
 }
