e2d6e7eb401e8c473d8ff9a42cba8e5316188046
Anton Sviridov <anton.sviridov@sourcegraph.com>
2024-09-12 12:45:19 +0100

    Only install coursier and sbt in docker container (#751)
    - Also remove unnecessary jar copying which caused CI flakiness

--- bin/docker-setup.sh
+++ bin/docker-setup.sh
@@ -2,7 +2,7 @@
 set -eux
 curl -fLo /usr/local/bin/coursier https://github.com/coursier/coursier/releases/download/v2.1.5/coursier
 chmod +x /usr/local/bin/coursier
-coursier setup --yes
+coursier setup --yes --apps coursier,sbt
 
 curl -fLo maven.zip https://archive.apache.org/dist/maven/maven-3/3.9.1/binaries/apache-maven-3.9.1-bin.zip 
 unzip -d /opt/maven maven.zip
