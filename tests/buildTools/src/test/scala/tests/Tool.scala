ee75d68e76a5ae74d2e7175532653fdcfa4b471d
jupblb <michal.kielbowicz@sourcegraph.com>
2026-03-23 16:16:58 +0100

    fix: bump Gradle8 test version from 8.7 to 8.10 to match CI

--- tests/buildTools/src/test/scala/tests/Tool.scala
+++ tests/buildTools/src/test/scala/tests/Tool.scala
@@ -36,7 +36,7 @@ object Tool {
   // See https://docs.gradle.org/current/userguide/compatibility.html
   sealed abstract class Gradle(version: String, support: JVMSupport)
       extends Tool("gradle", version, support)
-  case object Gradle8 extends Gradle("8.7", atMostJava(21))
+  case object Gradle8 extends Gradle("8.10", atMostJava(21))
   case object Gradle7 extends Gradle("7.6.3", atMostJava(17))
   case object Gradle6 extends Gradle("6.8.3", atMostJava(11))
   case object Gradle5 extends Gradle("5.6.4", atMostJava(11))
