01eafbc8b9e758477a5382eb6797b27c3638a860
jupblb <michal.kielbowicz@sourcegraph.com>
2026-05-25 20:43:54 +0200

    Expose packages.scip-java built via coursier bootstrap
    Adds a fixed-output derivation that runs `coursier bootstrap` against
    a pinned scip-java version from Maven Central, plus a thin wrapper that
    adds a Nix-provided JDK to PATH. Exposed as:
    
      nix build .#scip-java
      nix run   .#scip-java -- index ...
    
    Bump `scipJavaVersion` and re-run `nix build` to get the new
    outputHash to commit.

--- flake.nix
+++ flake.nix
@@ -18,6 +18,7 @@
       system:
       let
         pkgs = import nixpkgs { inherit system; };
+        lib = pkgs.lib;
         mkDevShell =
           jdk:
           pkgs.mkShellNoCC {
@@ -38,8 +39,64 @@
               yarn
             ];
           };
+
+        # A standalone scip-java launcher built via `coursier bootstrap`.
+        # The fixed-output derivation lets Nix cache the result; bump
+        # `scipJavaVersion` and rebuild to get a new outputHash.
+        scipJavaVersion = "0.12.3";
+        runtimeJdk = pkgs.jdk17;
+        scipJavaStandalone = pkgs.stdenvNoCC.mkDerivation {
+          pname = "scip-java-standalone";
+          version = scipJavaVersion;
+          dontUnpack = true;
+          dontFixup = true;
+          nativeBuildInputs = [ (pkgs.coursier.override ({ jre = runtimeJdk; })) ];
+          outputHashMode = "recursive";
+          outputHashAlgo = "sha256";
+          outputHash = "sha256-ifYIQQJftpwvDa7BUSL+OwfQ6W23l1Au9QP6tUmq0mQ=";
+          buildPhase = ''
+            export HOME="$TMPDIR/home"
+            export COURSIER_CACHE="$TMPDIR/coursier-cache"
+            export COURSIER_PROGRESS=0
+            export SSL_CERT_FILE="${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt"
+            export NIX_SSL_CERT_FILE="$SSL_CERT_FILE"
+            mkdir -p "$HOME" "$COURSIER_CACHE"
+            cs bootstrap \
+              "com.sourcegraph:scip-java_2.13:${scipJavaVersion}" \
+              -M com.sourcegraph.scip_java.ScipJava \
+              --standalone \
+              -f \
+              -o scip-java
+          '';
+          installPhase = ''
+            install -Dm755 scip-java "$out/bin/scip-java"
+          '';
+        };
+        scipJava = pkgs.stdenvNoCC.mkDerivation {
+          pname = "scip-java";
+          version = scipJavaVersion;
+          dontUnpack = true;
+          nativeBuildInputs = [ pkgs.makeWrapper ];
+          installPhase = ''
+            install -Dm755 ${scipJavaStandalone}/bin/scip-java "$out/bin/scip-java"
+            patchShebangs "$out/bin/scip-java"
+            wrapProgram "$out/bin/scip-java" \
+              --prefix PATH : ${lib.makeBinPath [ runtimeJdk ]}
+          '';
+          meta.mainProgram = "scip-java";
+        };
       in
       {
+        packages = {
+          default = scipJava;
+          scip-java = scipJava;
+        };
+
+        apps = {
+          default = flake-utils.lib.mkApp { drv = scipJava; };
+          scip-java = flake-utils.lib.mkApp { drv = scipJava; };
+        };
+
         checks = {
           actionlint = pkgs.runCommand "check-actionlint" { } ''
             ${pkgs.actionlint}/bin/actionlint ${./.github/workflows}/*.yml
