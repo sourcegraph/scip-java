30cfe2e0622761f123eeaf97bddf149ad2efd850
jupblb <michal.kielbowicz@sourcegraph.com>
2026-07-01 16:57:24 +0200

    Update Maven fixture expectation

--- scip-java/src/test/kotlin/tests/MavenBuildToolTest.kt
+++ scip-java/src/test/kotlin/tests/MavenBuildToolTest.kt
@@ -15,7 +15,7 @@ class MavenBuildToolTest : BuildToolHarness() {
                 expectedPackages =
                     """
                     |maven:com.sourcegraph:example:1.0-SNAPSHOT
-                    |maven:junit:junit:4.11
+                    |maven:junit:junit:4.13.1
                     |maven:org.hamcrest:hamcrest-core:1.3
                     |"""
                         .trimMargin(),
